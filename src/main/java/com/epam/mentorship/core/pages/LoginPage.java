771e6d0965c24536b974a47202162a749ec8e76c
AnnaRomanysh <anetswan@gmail.com>
2020-09-01 19:02:58 +0300

    add header test to suite

--- src/main/java/com/epam/mentorship/core/pages/LoginPage.java
+++ src/main/java/com/epam/mentorship/core/pages/LoginPage.java
@@ -6,9 +6,9 @@ import com.epam.mentorship.utils.annotations.RelativeUrl;
 @RelativeUrl(url = "/index.php?controller=authentication&back=my-account")
 public class LoginPage extends BasePage {
 
-
     @Override
     public String getPageName() {
         return "Login Page";
     }
+
 }
